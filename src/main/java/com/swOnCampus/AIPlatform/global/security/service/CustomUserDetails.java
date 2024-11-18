b54172d24726db0db299c4c6227c43d7b49a4b8e
kjh03 <kjh03124@naver.com>
2024-11-19 02:25:13 +0900

    CustomUserDetails, CustomUserDetails 작성

---
+++ src/main/java/com/swOnCampus/AIPlatform/global/security/service/CustomUserDetails.java
@@ -0,0 +1,59 @@
+package com.swOnCampus.AIPlatform.global.security.service;
+
+import com.swOnCampus.AIPlatform.domain.member.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private Member member;
+
+    public CustomUserDetails(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return member.getAuthorityList().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getType().name()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
