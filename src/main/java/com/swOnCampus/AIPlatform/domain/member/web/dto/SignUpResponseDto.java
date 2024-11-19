6f97e4f7cab901de381f5c02d5fc8536009932a2
= <=>
2024-11-20 02:09:18 +0900

    회원가입 응답 DTO 작성

--- src/main/java/com/swOnCampus/AIPlatform/domain/member/web/dto/SignUpResponseDto.java
+++ src/main/java/com/swOnCampus/AIPlatform/domain/member/web/dto/SignUpResponseDto.java
@@ -1,4 +1,18 @@
 package com.swOnCampus.AIPlatform.domain.member.web.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 public class SignUpResponseDto {
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class SignUpResponse {
+        private String name;
+        private String token;
+    }
 }
