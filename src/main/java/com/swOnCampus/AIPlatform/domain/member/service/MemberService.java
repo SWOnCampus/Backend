0a7c0ba48ab73572b5567fbeee1c66bc815f2c40
= <=>
2024-11-19 16:10:57 +0900

    이메일 인증 서비스 메서드 작성

--- src/main/java/com/swOnCampus/AIPlatform/domain/member/service/MemberService.java
+++ src/main/java/com/swOnCampus/AIPlatform/domain/member/service/MemberService.java
@@ -1,4 +1,6 @@
 package com.swOnCampus.AIPlatform.domain.member.service;
 
+
 public interface MemberService {
+    Boolean isExistEmail(String email);
 }
--- src/main/java/com/swOnCampus/AIPlatform/domain/member/service/MemberServiceImpl.java
+++ src/main/java/com/swOnCampus/AIPlatform/domain/member/service/MemberServiceImpl.java
@@ -1,11 +1,32 @@
 package com.swOnCampus.AIPlatform.domain.member.service;
 
+import com.swOnCampus.AIPlatform.domain.member.entity.Member;
 import com.swOnCampus.AIPlatform.domain.member.repository.MemberRepository;
+import com.swOnCampus.AIPlatform.domain.member.web.dto.EmailCheckRequestDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class MemberServiceImpl implements MemberService {
     MemberRepository memberRepository;
+
+
+    // Email 중복 검사 함수
+    @Override
+    public Boolean isExistEmail(String email) {
+        boolean findMember = memberRepository.existsByEmail(email);
+
+        if(findMember){
+            return true;
+        }
+
+        return false;
+    }
 
 
 }
