c3a4259df7d60057bf9407f82256c2ca7962f2e6
= <=>
2024-11-20 00:02:16 +0900

    사업자번호 인증 컨트롤러 코드 작성

--- src/main/java/com/swOnCampus/AIPlatform/domain/member/web/controller/MemberController.java
+++ src/main/java/com/swOnCampus/AIPlatform/domain/member/web/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.swOnCampus.AIPlatform.domain.member.web.controller;
 
 import com.swOnCampus.AIPlatform.domain.member.service.MemberService;
+import com.swOnCampus.AIPlatform.domain.member.web.dto.BusinessCheckRequestDto;
+import com.swOnCampus.AIPlatform.domain.member.web.dto.BusinessCheckResponseDto;
 import com.swOnCampus.AIPlatform.domain.member.web.dto.EmailCheckRequestDto;
 import com.swOnCampus.AIPlatform.domain.member.web.dto.EmailCheckResponseDto;
 import com.swOnCampus.AIPlatform.global.response.ApiResponse;
@@ -9,10 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -36,6 +35,23 @@ public class MemberController {
 
         ApiResponse<EmailCheckResponseDto.EmailCheckResponse> successResponse = ApiResponse.createSuccess(HttpStatus.OK.value(), responseDto, "이메일 인증이 성공적으로 완료되었습니다.");
         return ResponseEntity.ok(successResponse);
+    }
 
+    // 사업자 번호 인증
+    @PostMapping("/corporation-num-check")
+    public ResponseEntity<ApiResponse<?>> corporationCheck(@Valid @RequestBody BusinessCheckRequestDto.BusinessCheckRequest request){
+        if(!memberService.isExistCorporation(request.getBusinessNumber())){
+
+            ApiResponse<?> failResponse = ApiResponse.createFailWithoutData(HttpStatus.NO_CONTENT.value(), "존재하지 않는 사업자번호입니다.");
+
+            return ResponseEntity.badRequest().body(failResponse);
+        }
+
+        BusinessCheckResponseDto.BusinessCheckResponse responseDto = BusinessCheckResponseDto.BusinessCheckResponse.builder()
+                .businessNum(request.getBusinessNumber())
+                .build();
+
+        ApiResponse<BusinessCheckResponseDto.BusinessCheckResponse> successResponse = ApiResponse.createSuccess(HttpStatus.OK.value(), responseDto, "사업자번호 인증이 성공적으로 완료되었습니다.");
+        return ResponseEntity.ok(successResponse);
     }
 }
