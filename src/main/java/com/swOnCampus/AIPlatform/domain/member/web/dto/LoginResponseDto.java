2195d3ff48b3dc19d0b7f203501432e0e644a2e9
= <=>
2024-11-20 02:47:08 +0900

    로그인 응답 DTO 작성

--- src/main/java/com/swOnCampus/AIPlatform/domain/member/web/dto/LoginResponseDto.java
+++ src/main/java/com/swOnCampus/AIPlatform/domain/member/web/dto/LoginResponseDto.java
@@ -1,4 +1,16 @@
 package com.swOnCampus.AIPlatform.domain.member.web.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 public class LoginResponseDto {
+
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class LoginResponse {
+        private String name;
+        private String token;
+    }
 }
