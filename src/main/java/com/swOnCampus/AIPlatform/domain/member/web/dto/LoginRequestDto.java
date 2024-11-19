a6f90370480b7fb5b183c13c89073eedd42bcd99
= <=>
2024-11-20 02:47:14 +0900

    로그인 요청 DTO 작성

--- src/main/java/com/swOnCampus/AIPlatform/domain/member/web/dto/LoginRequestDto.java
+++ src/main/java/com/swOnCampus/AIPlatform/domain/member/web/dto/LoginRequestDto.java
@@ -1,4 +1,16 @@
 package com.swOnCampus.AIPlatform.domain.member.web.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 public class LoginRequestDto {
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class LoginRequest {
+        private String email;
+        private String password;
+    }
 }
