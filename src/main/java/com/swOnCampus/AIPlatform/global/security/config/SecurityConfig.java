28a4cc5ed1ce4d9079990b10122409c8b9544758
= <=>
2024-11-20 18:32:52 +0900

    permitted uri 수정

--- src/main/java/com/swOnCampus/AIPlatform/global/security/config/SecurityConfig.java
+++ src/main/java/com/swOnCampus/AIPlatform/global/security/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                         // swagger 관련
                         .requestMatchers("/v2/api-docs","/swagger-resources/**","/swagger-ui.html","/webjars/**","/swagger-ui/**","/v3/api-docs/**","/v2/swagger.json").permitAll()
                         .requestMatchers("/ws/**").permitAll()
-                        .requestMatchers("/", "/login","/signup").permitAll()
+                        .requestMatchers("/", "/login","/signup", "/corporation-num-check", "/email-check").permitAll()
                         .requestMatchers("/api/**").authenticated()
                         .anyRequest().authenticated()
                 )
