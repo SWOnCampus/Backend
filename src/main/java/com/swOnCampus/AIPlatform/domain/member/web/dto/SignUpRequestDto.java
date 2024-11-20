a99bdb1a541563c6c59bdd7f65040f140c8c9c24
= <=>
2024-11-20 23:48:40 +0900

    비밀번호 설정 조건을 특수문자, 영어, 숫자를 포함하여 8자리 이상으로 하기 위한 어노테이션 추가

--- src/main/java/com/swOnCampus/AIPlatform/domain/member/web/dto/SignUpRequestDto.java
+++ src/main/java/com/swOnCampus/AIPlatform/domain/member/web/dto/SignUpRequestDto.java
@@ -1,5 +1,6 @@
 package com.swOnCampus.AIPlatform.domain.member.web.dto;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,10 @@ public class SignUpRequestDto {
     @NoArgsConstructor
     public static class SignupRequest{
         private String email;
+        @Pattern(
+                regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]).{8,}$",
+                message = "비밀번호는 특수문자, 영어, 숫자를 포함하여 8자리 이상이어야 합니다."
+        )
         private String password;
         private String name;
         private String phone;
