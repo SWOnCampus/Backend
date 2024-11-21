624ea6eaf1821b59736fc2378168243f970e2ab9
= <=>
2024-11-21 16:16:56 +0900

    @LoginMember 어노테이션 사용을 위한 WebConfig 파일 내 addArgumentResolvers 메서드 작성

--- src/main/java/com/swOnCampus/AIPlatform/global/config/WebConfig.java
+++ src/main/java/com/swOnCampus/AIPlatform/global/config/WebConfig.java
@@ -1,10 +1,14 @@
 package com.swOnCampus.AIPlatform.global.config;
 
 import com.swOnCampus.AIPlatform.domain.member.repository.MemberRepository;
+import com.swOnCampus.AIPlatform.global.resolver.LoginMemberArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -20,5 +24,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowedMethods("*");
     }
-
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver(memberRepository));
+    }
 }
