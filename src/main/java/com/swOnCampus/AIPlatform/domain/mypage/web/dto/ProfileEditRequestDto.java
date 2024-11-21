b392e9d71f5442c6422c33516ed227ededd15701
= <=>
2024-11-21 16:19:07 +0900

    마이페이지 정보 수정을 위한 요청 DTO 작성

---
+++ src/main/java/com/swOnCampus/AIPlatform/domain/mypage/web/dto/ProfileEditRequestDto.java
@@ -0,0 +1,18 @@
+package com.swOnCampus.AIPlatform.domain.mypage.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class ProfileEditRequestDto {
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ProfileEditRequest {
+        private String email;
+        private String name;
+        private String phone;
+        private String corporationNum;
+    }
+}
