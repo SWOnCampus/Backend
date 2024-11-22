f382575967a9410984162a6707f9219dbcf482b9
= <=>
2024-11-22 19:44:06 +0900

    postgre설정

--- src/main/java/com/swOnCampus/AIPlatform/AiPlatformApplication.java
+++ src/main/java/com/swOnCampus/AIPlatform/AiPlatformApplication.java
@@ -2,12 +2,16 @@ package com.swOnCampus.AIPlatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
 @EnableJpaRepositories
 @SpringBootApplication
+@EntityScan(basePackages = {
+		"com/swOnCampus/AIPlatform/domain/member/entity"
+})
 public class AiPlatformApplication {
 
 	public static void main(String[] args) {
