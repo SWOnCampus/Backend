13706ea3bfc16020b4ed3c8e9991fc7c828f3dbc
minahYu <mina010311@naver.com>
2024-11-22 22:46:45 +0900

    feat: Consulting 요청 DTO 생성

---
+++ src/main/java/com/swOnCampus/AIPlatform/domain/consulting/web/dto/request/ConsultingRequest.java
@@ -0,0 +1,9 @@
+package com.swOnCampus.AIPlatform.domain.consulting.web.dto.request;
+
+public record ConsultingRequest(
+    String industry,
+    String companySize,
+    String painPoint
+) {
+
+}
