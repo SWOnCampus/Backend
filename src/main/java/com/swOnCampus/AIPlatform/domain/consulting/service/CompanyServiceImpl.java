ccafe6f6403b9d05d90e74beb245e444a9d8edf5
minahYu <mina010311@naver.com>
2024-11-23 15:36:17 +0900

    fix: 테이블 매핑 변경에 따른 saveCompanyInfo 메서드 수정

--- src/main/java/com/swOnCampus/AIPlatform/domain/consulting/service/CompanyServiceImpl.java
+++ src/main/java/com/swOnCampus/AIPlatform/domain/consulting/service/CompanyServiceImpl.java
@@ -1,11 +1,14 @@
 package com.swOnCampus.AIPlatform.domain.consulting.service;
 
 import com.swOnCampus.AIPlatform.domain.consulting.entity.Company;
-import com.swOnCampus.AIPlatform.domain.consulting.exception.ConsultingErrorCode;
 import com.swOnCampus.AIPlatform.domain.consulting.repository.CompanyRepository;
+import com.swOnCampus.AIPlatform.domain.consulting.repository.ConsultingRepository;
 import com.swOnCampus.AIPlatform.domain.consulting.web.dto.request.CompanyInfoRequest;
 import com.swOnCampus.AIPlatform.domain.consulting.web.dto.request.ConsultingRequest;
 import com.swOnCampus.AIPlatform.domain.consulting.web.dto.response.ConsultingResponse;
+import com.swOnCampus.AIPlatform.domain.member.entity.Member;
+import com.swOnCampus.AIPlatform.domain.member.repository.MemberRepository;
+import com.swOnCampus.AIPlatform.global.exception.CommonErrorCode;
 import com.swOnCampus.AIPlatform.global.exception.GlobalException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,8 @@ import org.springframework.stereotype.Service;
 public class CompanyServiceImpl implements CompanyService {
 
     private final CompanyRepository companyRepository;
+    private final MemberRepository memberRepository;
+    private final ConsultingRepository consultingRepository;
     private final ConsultingService consultingService;
 
     @Override
@@ -23,12 +28,11 @@ public class CompanyServiceImpl implements CompanyService {
         CompanyInfoRequest companyInfoRequest,
         boolean summary
     ) {
-        if (!companyRepository.existsByUserId(memberId)) {
-            throw new GlobalException(ConsultingErrorCode.NOT_EXIST_USER);
-        }
+        Member member = memberRepository.findById(memberId)
+            .orElseThrow(() -> new GlobalException(CommonErrorCode.NOT_EXIST_MEMBER));
 
         Company company = Company.builder()
-            .userId(memberId)
+            .member(member)
             .name(companyInfoRequest.name())
             .companySize(companyInfoRequest.companySize())
             .industry(companyInfoRequest.industry())
