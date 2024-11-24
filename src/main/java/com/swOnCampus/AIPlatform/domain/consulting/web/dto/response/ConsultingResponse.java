f31ac71823abc8eb624f48b69c307d444828902e
minahYu <mina010311@naver.com>
2024-11-24 20:48:41 +0900

    docs: 기업정보 입력 API 관련 Swagger 명세 추가

--- src/main/java/com/swOnCampus/AIPlatform/domain/consulting/web/dto/response/ConsultingResponse.java
+++ src/main/java/com/swOnCampus/AIPlatform/domain/consulting/web/dto/response/ConsultingResponse.java
@@ -1,6 +1,9 @@
 package com.swOnCampus.AIPlatform.domain.consulting.web.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record ConsultingResponse(
+    @Schema(description = "컨설팅 요약 내용", example = "summary")
     String summary
 ) {
 
