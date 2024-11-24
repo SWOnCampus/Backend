7f67b880cd5d1cfab006c066ad48839ddc253207
minahYu <mina010311@naver.com>
2024-11-24 20:34:11 +0900

    docs: 컨설팅 결과 PDF API 관련 Swagger 명세 추가

--- src/main/java/com/swOnCampus/AIPlatform/domain/report/web/controller/ReportController.java
+++ src/main/java/com/swOnCampus/AIPlatform/domain/report/web/controller/ReportController.java
@@ -5,6 +5,11 @@ import com.swOnCampus.AIPlatform.domain.report.service.ReportService;
 import com.swOnCampus.AIPlatform.domain.report.web.dto.ReportingResponse;
 import com.swOnCampus.AIPlatform.global.annotation.LoginMember;
 import com.swOnCampus.AIPlatform.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "컨설팅 PDF 생성 API", description = "컨설팅 결과 PDF 생성 관련 API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/report")
@@ -20,6 +26,20 @@ public class ReportController {
 
     private final ReportService reportService;
 
+    @Operation(summary = "컨설팅 PDF API 요청", description = "컨설팅 결과 PDF 생성 API 요청")
+    @ApiResponses(value = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "COMMON200",
+            description = "요청 성공",
+            content = {
+                @Content(
+                    schema = @Schema(
+                        implementation = ReportingResponse.class
+                    )
+                )
+            }
+        )
+    })
     @GetMapping()
     public ResponseEntity<ApiResponse<?>> createReportingPdf(
         @LoginMember Member member,
@@ -28,9 +48,11 @@ public class ReportController {
         ReportingResponse reportingResponse = reportService.createReportingPdf(
             member.getMemberId(), companyId);
 
-        ApiResponse<ReportingResponse> response = ApiResponse.createSuccess(HttpStatus.OK.value(),
+        ApiResponse<ReportingResponse> response = ApiResponse.createSuccess(
+            HttpStatus.OK.value(),
             reportingResponse,
-            "PDF 파일이 생성되었습니다.");
+            "PDF 파일이 생성되었습니다."
+        );
 
         return ResponseEntity.ok(response);
     }
--- src/main/java/com/swOnCampus/AIPlatform/domain/report/web/dto/ReportingResponse.java
+++ src/main/java/com/swOnCampus/AIPlatform/domain/report/web/dto/ReportingResponse.java
@@ -1,10 +1,13 @@
 package com.swOnCampus.AIPlatform.domain.report.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record ReportingResponse(
+    @Schema(description = "컨설팅 요약 내용", example = "컨설팅 요약")
     String summary,
+    @Schema(description = "컨설팅 전체 내용 PDF", example = "byte")
     String pdf
 ) {
 
