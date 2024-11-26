f562c578974331c9cc072dbf9c3436cd172f8c54
= <=>
2024-11-26 19:18:24 +0900

    요약본이 아닌 전체 컨설팅 생성을 위한 컨트롤러 코드 작성

--- src/main/java/com/swOnCampus/AIPlatform/domain/consulting/web/controller/ConsultingController.java
+++ src/main/java/com/swOnCampus/AIPlatform/domain/consulting/web/controller/ConsultingController.java
@@ -1,7 +1,10 @@
 package com.swOnCampus.AIPlatform.domain.consulting.web.controller;
 
 import com.swOnCampus.AIPlatform.domain.consulting.service.CompanyService;
+import com.swOnCampus.AIPlatform.domain.consulting.service.ConsultingAllService;
 import com.swOnCampus.AIPlatform.domain.consulting.web.dto.request.CompanyInfoRequest;
+import com.swOnCampus.AIPlatform.domain.consulting.web.dto.request.ConsultingAllRequestDto;
+import com.swOnCampus.AIPlatform.domain.consulting.web.dto.response.ConsultingAllResponseDto;
 import com.swOnCampus.AIPlatform.domain.consulting.web.dto.response.ConsultingResponse;
 import com.swOnCampus.AIPlatform.domain.member.entity.Member;
 import com.swOnCampus.AIPlatform.global.annotation.LoginMember;
@@ -21,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConsultingController {
 
     private final CompanyService companyService;
+    private final ConsultingAllService consultingAllService;
 
     @PostMapping()
     public ResponseEntity<ApiResponse<?>> createConsulting(
@@ -34,6 +38,15 @@ public class ConsultingController {
         ApiResponse<ConsultingResponse> response = ApiResponse.createSuccess(HttpStatus.OK.value(),
             result, "컨설팅 결과가 생성되었습니다.");
 
+        return ResponseEntity.ok(response);
+    }
+
+    // 컨설팅 결과 전체 상세 보기 (요약 x)
+    @PostMapping("/all")
+    public ResponseEntity<ApiResponse<ConsultingAllResponseDto.ConsultingAllResponse>> getAllConsulting(@LoginMember Member member, @RequestBody ConsultingAllRequestDto.ConsultingAllRequest request){
+        ConsultingAllResponseDto.ConsultingAllResponse responseDto = consultingAllService.getConsultingAll(member.getMemberId(), request);
+
+        ApiResponse<ConsultingAllResponseDto.ConsultingAllResponse> response = ApiResponse.createSuccess(HttpStatus.OK.value(), responseDto, "컨설팅 세부 정보 불러오기를 완료하였습니다.");
         return ResponseEntity.ok(response);
     }
 }
