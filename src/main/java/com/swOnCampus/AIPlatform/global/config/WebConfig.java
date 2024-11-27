90e80dd6fc48c2f8ef95bf5bfa67a52c119ae39a
= <=>
2024-11-28 00:07:39 +0900

    Cors 설정 변경

--- src/main/java/com/swOnCampus/AIPlatform/global/config/WebConfig.java
+++ src/main/java/com/swOnCampus/AIPlatform/global/config/WebConfig.java
@@ -16,15 +16,6 @@ public class WebConfig implements WebMvcConfigurer {
     private final MemberRepository memberRepository;
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowCredentials(true)
-                .maxAge(3600)
-                .allowedHeaders("*")
-                .allowedMethods("*");
-    }
-    @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberArgumentResolver(memberRepository));
     }
--- src/main/java/com/swOnCampus/AIPlatform/global/security/config/SecurityConfig.java
+++ src/main/java/com/swOnCampus/AIPlatform/global/security/config/SecurityConfig.java
@@ -14,6 +14,10 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -47,6 +51,17 @@ public class SecurityConfig {
                 .addFilterBefore(new JwtTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
                 .userDetailsService(customUserDetailService);
 
+        http
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOriginPatterns(Collections.singletonList("*"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setExposedHeaders(Arrays.asList("Authorization"));
+                    config.setMaxAge(3600L);
+                    return config;
+                }));
 
         return http.build();
     }
